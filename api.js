826bf494926f9f8353c30b26be49075e21ee060d
Kartikey Dwivedi <kartikeydwivedipara@gmail.com>
2020-12-06 01:40:40 +0530

    wip: progress

---
+++ api.js
@@ -0,0 +1,38 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
+var serverURL = "https://api.funtranslations.com/translate/groot.json";
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+// function errorHandler(error) {
+//     console.log("error occured", error);
+//     alert("Something wrong with server! Try again after some time.")
+// }
+
+function clickHandler() {
+    //taking input
+    var inputText = txtInput.value; 
+
+
+    //calling server for processing..
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something wrong with server! Try again after some time.")
+}
